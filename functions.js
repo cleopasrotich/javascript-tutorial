cd409e6e72e755abd2dc216910d3b9a718e65839
cleopasrotich <cleopasrotich81@gmail.com>
2018-06-08 09:24:33 +0300

    initial commit

---
+++ functions.js
@@ -0,0 +1,9 @@
+function isPrime(integer){
+	for (var x = 2; x < integer; x++) {
+		if (integer % x === 0) {
+			console.log(integer + "is divisible by " + x);
+			return false;
+		}
+	}
+	return true;
+}
